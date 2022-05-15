397081e3d9adf118db78aeebc5e8d609b6851c66
BenLopez00 <lop.ben00@gmail.com>
2022-05-15 02:44:36 -0300

    Cambios esteticos

--- procesamiento.js
+++ procesamiento.js
@@ -1,50 +1,18 @@
 //Proceso los datos y los mando a la ultima funcion
+let tasaDelMedioDePago = 0;
 function Procesamiento(){
     //agarro el boton de submit
     let botonMandarDatos = document.getElementById("calcular");
     //le doy un evento
     botonMandarDatos.addEventListener("click", ()=>{
-        //onclick event
         //tomo el ultimo input del menu desplegable que contiene las tasas de cada medio de pago
         let tipoTarjeta = parseInt(document.getElementById("tipoTarjeta").value);
-        let tasaDelMedioDePago = 0;
-        (tipoTarjeta>0 && tipoTarjeta<5) && seleccionarMedioDePago();
-        function seleccionarMedioDePago(){
-            const tasaDeTarjetas = JSON.parse (localStorage.getItem("tasaDeInteres"));
-            const {visa, mastercard, cabal, amex} = tasaDeTarjetas;
-            switch (tipoTarjeta) {
-                case 1:
-                    tasaDelMedioDePago = visa;
-                    break;
-                case 2:
-                    tasaDelMedioDePago = mastercard;
-                    break;
-                case 3:
-                    tasaDelMedioDePago = cabal;
-                    break;
-                case 4:
-                    tasaDelMedioDePago = amex;
-                    break;
-            }
-            console.log(`TASA del medio de pago con operador${tasaDelMedioDePago}`);
-        }
+        seleccionarMedioDePago(tipoTarjeta);
         //Sumo los precios del array
         listaDePrecios.forEach((precioItem)=>precioIngresado+=precioItem);
         console.log (precioIngresado);
         //mando los datos
-
-
-
-
-
-
         ((!isNaN(precioIngresado)) && (!isNaN(valorCuotas)) && (!isNaN(tasaDelMedioDePago))) ?  precioCalculo(precioIngresado, valorCuotas, tasaDelMedioDePago)  : ((Toastify({text: "Ingresa y Carga los numeros antes de seguir",duration: 4000,style: {background: 'linear-gradient(90deg, rgba(11,0,182,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)', fontFamily: "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif"}}).showToast()) & (valorCuotas=0));
-
-
-
-
-
-
     })
     //agarro el boton para reiniciar
     let botonReiniciar = document.getElementById("reiniciar");
@@ -55,4 +23,20 @@ function Procesamiento(){
     })
 }
 
-
+function seleccionarMedioDePago(tipoTarjeta){
+    if ((tipoTarjeta>0) && (tipoTarjeta<5)){
+        const tasaDeTarjetas = JSON.parse (localStorage.getItem("tasaDeInteres"));
+        const {visa, mastercard, cabal, amex} = tasaDeTarjetas;
+        switch (tipoTarjeta) {
+            case 1:
+                return tasaDelMedioDePago = visa;
+            case 2:
+                return tasaDelMedioDePago = mastercard;
+            case 3:
+                return tasaDelMedioDePago = cabal;
+            case 4:
+                return tasaDelMedioDePago = amex;
+        }
+        console.log(`TASA del medio de pago con operador${tasaDelMedioDePago}`);
+    }
+}
